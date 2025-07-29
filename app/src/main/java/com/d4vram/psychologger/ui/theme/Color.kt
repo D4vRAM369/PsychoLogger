c9efe762e9ad8641a728bbb694ac53fe0b057672
D4vRAM <d4vram369@proton.me>
2025-07-29 23:06:22 +0100

    Saved game

--- app/src/main/java/com/d4vram/psychologger/ui/theme/Color.kt
+++ app/src/main/java/com/d4vram/psychologger/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.d4vram.psychologger.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Colores por defecto (auto-generados)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Colores personalizados para el tema
+val PrimaryPurple = Color(0xFF8B5CF6)
+val SecondaryPink = Color(0xFFEC4899)
+val DarkBg        = Color(0xFF0F0F23)
+val DarkerBg      = Color(0xFF080813)
+val TextLight     = Color(0xFFFFFFFF)
+val AccentCyan    = Color(0xFF06B6D4)
+val AccentOrange  = Color(0xFFF59E42)
+val CardBg        = Color(0xFF1A1A2E)
