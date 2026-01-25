6350502f4749334545c7ad559415ad3e5e68a889
D4vRAM369 <d4vram369@proton.me>
2026-01-25 10:52:52 +0000

    fix: bump version 1.2.2 → 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.d4vram.psychologger"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.2.2"
+        versionCode = 6
+        versionName = "1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
